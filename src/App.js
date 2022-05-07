36cbcd483ef57dd57a27a116d7c7cbd6c87a4000
Prasad <prasadj007official@gmail.com>
2022-05-07 15:54:21 +0530

    Added flex demo in react

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+
+function App() {
+  return (
+          <body>
+            <div class="wrapper">
+              <header class="header">
+                <h1>Article Heading</h1>
+              </header>
+              <article class="main">
+                <p>HTML paragraph or HTML p tag is used to define a paragraph in a webpage. Let's take a simple example to see how it work. It is a notable point that a browser itself add an empty line before and after a paragraph. If you put a lot of spaces inside the HTML p tag, browser removes extra spaces and extra line while displaying the page. The browser counts.
+               </p>
+                </article>
+                <aside class="aside aside1">
+                  <h1>Aside1</h1>
+                  </aside>
+                <aside class="aside aside2">
+                  <h1>Aside2</h1>
+                  </aside>
+                  <footer class="footer">
+                  <h1>Footer
+                    </h1>
+                  </footer>  
+            </div>
+            </body>
+  );
+}
+
+export default App;
